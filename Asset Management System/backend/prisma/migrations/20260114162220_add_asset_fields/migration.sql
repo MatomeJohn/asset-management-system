a027a590d1a2bf26c253bc5409b2a4a41151462f
MatomeJohn <matomejsekgotla@gmail.com>
2026-02-02 21:14:11 +0200

    Remove SQLite migrations - switching to PostgreSQL

--- Asset Management System/backend/prisma/migrations/20260114162220_add_asset_fields/migration.sql
+++
@@ -1,4 +0,0 @@
--- AlterTable
-ALTER TABLE "assets" ADD COLUMN "deviceName" TEXT;
-ALTER TABLE "assets" ADD COLUMN "serialNumber" TEXT;
-ALTER TABLE "assets" ADD COLUMN "userAssigned" TEXT;
--- Asset Management System/backend/prisma/migrations/20260115153322_add_asset_specs_fields/migration.sql
+++
@@ -1,4 +0,0 @@
--- AlterTable
-ALTER TABLE "assets" ADD COLUMN "processor" TEXT;
-ALTER TABLE "assets" ADD COLUMN "ram" TEXT;
-ALTER TABLE "assets" ADD COLUMN "storage" TEXT;
